60a8ce13247a108e88a9607fd24b51f4e41c373c
Kamil Sasin <ksasin3@gmail.com>
2022-05-04 13:25:07 +0200

    added constants and annotations

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,39 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+    private static final int TAB_SIZE = 12;
+    private static final int TOTAL = -1;
 
-	public int total = -1;
+    private final int[] numbers = new int[TAB_SIZE];
 
-	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
-	}
+    private int total = TOTAL;
 
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return numbers[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return numbers[total--];
-					}
+    public int getTotal() { return total; }
 
+    public void setTotal(int t) { total = t; }
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == TOTAL;
+    }
+
+    public boolean isFull() {
+        return total == 11;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return TOTAL;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return TOTAL;
+        return numbers[total--];
+    }
 }
