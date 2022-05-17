687271b63402456c24973a87b66ebb0b31938147
Mateusz Sikorski <235968@edu.p.lodz.pl>
2022-05-17 23:25:10 +0200

    1.5.6 (dodanie dokumentacji klasy Node)

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,6 +1,12 @@
 package edu.kis.vh.nursery.list;
 
 public class Node {
+	/**
+	 * private final int value- integer value held in the node
+	 * private Node prev- next node on the list
+	 * private Node next- previous node on the list
+	 * all getX() and setX() methods- methods used to get or upgrade values in the nodes
+	 */
 	private final int value;
 	private Node prev;
 	private Node next;
